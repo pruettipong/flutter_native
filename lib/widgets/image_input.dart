6e01fb2dc1df5ebc279086be29331007a2883c99
pruettipong <noobyxug@gmail.com>
2025-02-26 14:33:42 +0700

    Update

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,15 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<ImageInput> createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyWidgetState extends State<ImageInput> {
+  File? _selectedImage;
+takePicture() async {
+  final imagePicker = ImagePicker();
+  final pickedImage =
+      await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+  if (pickedImage == null) {
+    return;
+  }
+
+  setState(() {
+    _selectedImage = File(pickedImage.path);
+  });
+}
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.white)
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: _selectedImage != null
+          ? GestureDetector(
+              onTap: takePicture, // Allow retaking an image by tapping
+              child: Image.file(
+                _selectedImage!,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            )
+          : TextButton.icon(
+              icon: const Icon(Icons.camera),
+              label: const Text('Take Picture'),
+              onPressed: takePicture,
+            ),
+    );
   }
 }
